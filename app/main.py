10e64c94c7d5ee22927710e01ae318f36f81cba1
West-Tiwan <xyzmohit.singh@gmail.com>
2024-05-31 10:04:13 +0530

    command not found

--- app/main.py
+++ app/main.py
@@ -4,13 +4,17 @@ import sys
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     #print("Logs from your program will appear here!")
-
+    valid_commands = []
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
     sys.stdout.flush()
 
     # Wait for user input
-    input()
+    command = input()
+    if command in valid_commands:
+        print('hello')
+    else:
+        print((f"{command}: command not found\r\n").replace('\r\n', '\n'))
 
 
 if __name__ == "__main__":
