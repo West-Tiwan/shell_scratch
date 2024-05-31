33e5865234a07fa4db4db81030bbacf2f48bbca5
West-Tiwan <xyzmohit.singh@gmail.com>
2024-05-31 10:55:28 +0530

    REPL works

--- app/main.py
+++ app/main.py
@@ -5,16 +5,19 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     #print("Logs from your program will appear here!")
     valid_commands = []
-    # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
 
-    # Wait for user input
-    command = input()
-    if command in valid_commands:
-        print('hello')
-    else:
-        print((f"{command}: command not found\r\n").replace('\r\n', '\n'))
+    #REPL loop
+    while True:
+        sys.stdout.write(f"$ ")
+        sys.stdout.flush()
+        # Wait for user input
+        command = input()
+        if command in valid_commands:
+            sys.stdout.write('hello')
+        else:
+            sys.stdout.write((f"{command}: command not found\r\n").replace('\r\n', '\n'))
+            continue
+
 
 
 if __name__ == "__main__":
