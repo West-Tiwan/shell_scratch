814006a5fb47df0e5f06ce82b1b888dba23341e0
West-Tiwan <xyzmohit.singh@gmail.com>
2024-05-31 10:58:15 +0530

    exit 0 works

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import sys
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     #print("Logs from your program will appear here!")
-    valid_commands = []
+    valid_commands = ["exit 0"]
 
     #REPL loop
     while True:
@@ -13,7 +13,8 @@ def main():
         # Wait for user input
         command = input()
         if command in valid_commands:
-            sys.stdout.write('hello')
+            if command == "exit 0":
+                sys.exit(0)
         else:
             sys.stdout.write((f"{command}: command not found\r\n").replace('\r\n', '\n'))
             continue
