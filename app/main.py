a940bc353c4e7173363d939df3ad07e85536eafe
West-Tiwan <xyzmohit.singh@gmail.com>
2024-06-03 13:02:45 +0530

    exeecute sub processes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+import subprocess
 def main():
     valid_commands = ["exit","echo","type"]
     PATH = os.environ.get('PATH')
@@ -28,10 +28,26 @@ def main():
             elif cmd not in valid_commands:
                 sys.stdout.write(f"{command.split(" ")[1]} not found\n")
         else:
-            sys.stdout.write(f"{command}: command not found\n")
-            sys.stdout.flush()
+            paths = PATH.split(":")
+            cmd_path = None
+            for path in paths:
+                if os.path.isfile(f"{path}/{command.split(" ")[0]}"):
+                    cmd_path = f"{path}/{command.split(" ")[0]}"
 
+            if cmd_path:
+                cmd_to_exe = cmd_path + " " + command.split( command.split(" ")[0] + " " )[1]
+                subprocess.run(cmd_to_exe, check=True)
 
+            elif os.path.isfile(command.split(" ")[0]):
+                cmd_to_exe = command
+                os.system(cmd_to_exe)
+                # subprocess.run(cmd_to_exe, check=True)
+
+            else:
+
+                # print(command.split(" ")[0])
+                # print(list_path_command)
+                print(command + ": command not found")
 
 if __name__ == "__main__":
     main()
